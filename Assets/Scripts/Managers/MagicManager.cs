20dad2dbcfa96f4c3139a79369336f169db91d97
axionicm4tt3r <marcelroodt@gmail.com>
2017-03-18 23:39:01 +0200

    Fixed Commit
    These files were meant to be checked in.

--- Assets/Scripts/Managers/MagicManager.cs
+++ Assets/Scripts/Managers/MagicManager.cs
@@ -1,40 +1,39 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class MagicManager : MonoBehaviour
 {
     public SpellBooleanPair[] spellList;
+    public Transform readySpellPrefab = null;
 
     private const bool ALL_SPELLS_UNLOCKED = true;
 
-    void Start()
+    public bool CheckValidSpellInputs(string inputString, Transform targetSpawnPosition)
     {
-
-    }
-
-    void Update()
-    {
-
-    }
-
-    public bool Cast(string inputString, Vector3 targetSpawnPosition)
-    {
-        //if (string.IsNullOrEmpty(inputString))
-       //     return false;
-
         foreach (SpellBooleanPair pair in spellList)
         {
-            if (inputString.Contains(pair.spellPrefab.GetComponent<ISpell>().inputString))
+            if (inputString.Contains(pair.spellPrefab.GetComponent<ISpell>().inputString) && (pair.unlocked || ALL_SPELLS_UNLOCKED))
             {
-                GameObject.Instantiate(pair.spellPrefab, targetSpawnPosition, Quaternion.identity);
+                readySpellPrefab = pair.spellPrefab;
+                CastSpell(targetSpawnPosition);
                 return true;
             }
         }
-
         return false;
+    }
+
+    private void CastSpell(Transform targetSpawnPosition)
+    {
+        //Casting logic and animations go over here probably
+        if (readySpellPrefab != null)
+        {
+            GameObject.Instantiate(readySpellPrefab, targetSpawnPosition.position, targetSpawnPosition.rotation);
+            readySpellPrefab = null;
+        }
+        else
+        {
+            //Play no magic spell sound
+        }
     }
 
     [Serializable]
@@ -43,35 +42,4 @@ public class MagicManager : MonoBehaviour
         public Transform spellPrefab;
         public bool unlocked;
     }
-}
-
-public interface ISpell
-{
-    string name { get; set; }
-    string inputString { get; set; }
-    Element element { get; set; }
-    SpellType type { get; set; }
-    float damage { get; set; }
-    float range { get; set; }
-    float aoe { get; set; }
-
-    void Trigger();
-}
-
-//This should be an interface itself or something to inherit from
-public enum Element
-{
-    Earth,
-    Fire,
-    Water,
-    Wind
-}
-
-public enum SpellType
-{
-    AoE,
-    Cone,
-    Projectile,
-    Ray,
-    Self
 }
---
+++ Assets/Scripts/Spells/ISpell.cs
@@ -0,0 +1,32 @@
+﻿public interface ISpell
+{
+    string spellName { get; set; }
+    string inputString { get; set; }
+    Element element { get; set; }
+    SpellType type { get; set; }
+    float damage { get; set; }
+    float range { get; set; }
+    float aoe { get; set; }
+    float speed { get; set; }
+
+    //How the spell affects the world
+    void Trigger();
+}
+
+//This should be an interface itself or something to inherit from
+public enum Element
+{
+    Earth,
+    Fire,
+    Water,
+    Wind
+}
+
+public enum SpellType
+{
+    AoE,
+    Cone,
+    Projectile,
+    Ray,
+    Self
+}

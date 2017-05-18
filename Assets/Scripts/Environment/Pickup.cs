069b2457c2ea4bfdbe1e476884419b18eadd9829
Mohamed Carrim <crrmoh003@myuct.ac.za>
2017-05-18 19:39:28 +0200

    Basic test level, pickup script
    Created basic (incomplete) level with some platforming elements and
    enemies.
    
    Added a script for pickup items.

---
+++ Assets/Scripts/Environment/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == Helpers.Tags.Player)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

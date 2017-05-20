8d2cdd962f1f926a5fa0834cc2cc4f3d0c1cb39d
Mohamed Carrim <crrmoh003@myuct.ac.za>
2017-05-20 13:40:55 +0200

    Player health, enemy damage deal
    Player now has health and gets sent to checkpoint when health reaches 0.
    
    Added enemy prefab that deals damage when bumped into.
    
    Minor improvements to code and level.

---
+++ Assets/Scripts/Enemy/EnemyBumpTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBumpTrigger : MonoBehaviour {
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == Helpers.Tags.Player)
+        {
+            collider.gameObject.GetComponent<PlayerStatus>().BumpedEnemy();
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class PlayerStatus : MonoBehaviour {
     public int Health = 3;
     public int Mana = 10;
     private Checkpoint checkpoint;
+    private int MaxHealth = 3;
     
 	void Start () {
 		
@@ -18,15 +20,32 @@ public class PlayerStatus : MonoBehaviour {
 
     public void OutOfBounds()
     {
-        print("You fell");
-        transform.position = checkpoint.transform.position;
+        Respawn();
+    }
+
+    public void BumpedEnemy()
+    {
+        TakeDamage(1);
     }
 
     public void CheckpointTouched(Checkpoint checkpoint)
     {
-        print("checkpoint touched!");
         if(this.checkpoint != null)
             this.checkpoint.SetInactive();
         this.checkpoint = checkpoint;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        print("ouch");
+        Health -= damage;
+        if (Health <= 0)
+            Respawn();
+    }
+
+    private void Respawn()
+    {
+        Health = MaxHealth;
+        transform.position = checkpoint.transform.position;
     }
 }

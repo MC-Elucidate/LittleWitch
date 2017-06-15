44b12054245226024557554ed98edce6e4f86012
Mohamed Carrim <crrmoh003@myuct.ac.za>
2017-06-15 20:56:29 +0200

    Jak Camera v1
    Made camera work like Jak camera. First version.
    Needs some refining.

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -19,10 +19,13 @@ public class CameraScript : MonoBehaviour
     public float mouseSensitivity = 50f;
     public float controllerSensitivity = 180f;
     private float timeEffect = 1;
+    private bool backToCharacter = false;
+    private float yRangeLimit = 3.5f;
 
     private Transform lookTarget;
     private CameraPositionPivotScript pivotTarget;
     private PlayerStatus playerStatus;
+    private PlayerMovementScript playerMovement;
     public Vector3 distanceFromTarget = new Vector3(0f, 0f, -4f);
 
     private const float PITCH_MAX_ANGLE = 45.0f;
@@ -33,7 +36,9 @@ public class CameraScript : MonoBehaviour
         lookTarget = GameObject.FindGameObjectWithTag(Helpers.Tags.CameraFollowTarget).transform;
         pivotTarget = GameObject.FindGameObjectWithTag(Helpers.Tags.CameraPositionPivot).GetComponent<CameraPositionPivotScript>();
         playerStatus = GameObject.FindGameObjectWithTag(Helpers.Tags.Player).GetComponent<PlayerStatus>();
+        playerMovement = GameObject.FindGameObjectWithTag(Helpers.Tags.Player).GetComponent<PlayerMovementScript>();
         state = CameraMode.Free;
+        transform.position = lookTarget.position + transform.rotation * distanceFromTarget;
     }
 
     void LateUpdate()
@@ -42,22 +47,40 @@ public class CameraScript : MonoBehaviour
         {
             if (yawInput != 0)
                 transform.RotateAround(lookTarget.position, Vector3.up, DeltaTime * yawInput);
+            
+            Vector3 lerpDestination = lookTarget.position;
+            bool cameraInYRange = Mathf.Abs(lerpDestination.y - transform.position.y) < yRangeLimit;
+            bool playerInAir = (playerMovement.velocity.y > 0 || (playerMovement.velocity.y < 0 && !playerMovement.isGrounded));
 
-            if (pitchInput != 0)
+            if (playerInAir)
             {
-                Vector3 objRotation = transform.rotation.eulerAngles;
-                float oldPitch = objRotation.x > 180 ? objRotation.x - 360 : objRotation.x;
-                float newPitch = oldPitch + (pitchInput * DeltaTime);
-                float clampedPitch = Mathf.Clamp(newPitch, PITCH_MIN_ANGLE, PITCH_MAX_ANGLE);
-                transform.localEulerAngles = new Vector3(clampedPitch, objRotation.y, objRotation.z);
+                if (cameraInYRange && !backToCharacter)
+                {
+                    lerpDestination.y = transform.position.y;
+                }
+                else
+                    backToCharacter = true;
             }
-            transform.position = Vector3.Lerp(transform.position, lookTarget.position + transform.rotation * distanceFromTarget, aimSmooth);
+            else if(Mathf.Abs(lookTarget.position.y - transform.position.y) < 0.15f)
+            {
+                backToCharacter = false;
+            }
+
+            Quaternion lerpAngle = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+            transform.position = Vector3.Lerp(transform.position, lerpDestination + lerpAngle * distanceFromTarget, 0.2f);
+
+            Quaternion currentRot = transform.rotation;
+            transform.LookAt(lerpDestination);
+            Quaternion destinationRot = transform.rotation;
+            transform.rotation = Quaternion.Lerp(currentRot, destinationRot, 0.2f);
+
+
         }
         else if (state == CameraMode.Aim)
         {
             transform.position = Vector3.Lerp(transform.position, pivotTarget.GetTagetPosition(), aimSmooth);
+            transform.LookAt(lookTarget.position);
         }
-        transform.LookAt(lookTarget.position);
     }
 
     private void EnterAimMode() { if(state != CameraMode.Aim) state = CameraMode.Aim; }

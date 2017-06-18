6352564c11a790a8e3ca6783a46d2301a5316338
Mohamed Carrim <crrmoh003@myuct.ac.za>
2017-06-18 22:20:30 +0200

    Basic Looping BGM
    Added a BGM that loops at a specific point in the track.
    Turned off PlayOnAwake on the AudioSource of the BGM so we don't hear it
    everytime. Enable it to hear the music when you run the game.

---
+++ Assets/Scripts/Camera/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    AudioSource source;
+
+	void Start () {
+        source = gameObject.GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        if (source.time >= 60.9f)
+            source.time = 0.9f;
+	}
+}

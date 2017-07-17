d73e99e28af0e7447ae93198c613e6e387db1407
Mohamed Carrim <crrmoh003@myuct.ac.za>
2017-07-17 20:14:50 +0200

    Switch and triggerable objects
    Added base code for switch and triggerable objects.
    A triggerable object must implement the TriggerableObject interface. The
    GameObject must then be added to the switche's array of objects to
    Trigger,

---
+++ Assets/Scripts/Environment/Props/SwitchProp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchProp : ChemistryObject {
+
+    private bool active = false;
+    private TriggerableObject[] objectsToTrigger;
+    public GameObject[] connectedObjects;
+
+    #region ElementInteractions
+    protected override void EarthInteraction()
+    {
+    }
+
+    protected override void FireInteraction()
+    {
+    }
+
+    protected override void WaterInteraction()
+    {
+    }
+
+    protected override void WindInteraction()
+    {
+    }
+    #endregion
+
+    protected override void TakeDamage(float damage)
+    {
+        active = !active;
+        foreach (TriggerableObject objectToTrigger in objectsToTrigger)
+        {
+            objectToTrigger.Trigger(active);
+        }
+    }
+
+    void Start () {
+        objectsToTrigger = new TriggerableObject[connectedObjects.Length];
+        for (int i = 0; i < connectedObjects.Length; i++)
+        {
+            objectsToTrigger[i] = connectedObjects[i].GetComponent<TriggerableObject>();
+        }
+	}
+}

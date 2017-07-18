d579c45008ab14a38ed6c01f2ba4c60bf41b2686
Mohamed Carrim <crrmoh003@myuct.ac.za>
2017-07-18 20:06:08 +0200

    AI for Bouncy Creatures
    Added a basic script that allows a navmesh agent to patrol points with a
    waiting time at each point.
    Used for bouncy creatures to let them move between desired points.

---
+++ Assets/Scripts/Environment/Creatures/PatrolPointsCreature.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolPointsCreature : MonoBehaviour {
+
+    public Vector3[] PatrolPoints;
+    public float WaitTime;
+    private NavMeshAgent navAgent;
+    private int currentPointIndex;
+    private bool isMoving;
+
+    void Start () {
+        navAgent = gameObject.GetComponent<NavMeshAgent>();
+        if (PatrolPoints.Length > 1)
+        {
+            navAgent.SetDestination(PatrolPoints[0]);
+            navAgent.Resume();
+            currentPointIndex = 0;
+            isMoving = true;
+        }
+        else
+        {
+            navAgent.Stop();
+            isMoving = false;
+        }
+    }
+	
+	void Update () {
+        if (isMoving && navAgent.remainingDistance <= navAgent.stoppingDistance)
+        {
+            isMoving = false;
+            navAgent.Stop();
+            Invoke("SetNextDestinationPoint", WaitTime);
+        }
+	}
+
+    void SetNextDestinationPoint()
+    {
+        ++currentPointIndex;
+        if (currentPointIndex >= PatrolPoints.Length)
+            currentPointIndex = 0;
+        navAgent.SetDestination(PatrolPoints[currentPointIndex]);
+        navAgent.Resume();
+        isMoving = true;
+    }
+}

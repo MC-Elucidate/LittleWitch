5199aadaa000b2f874ebb9e60e4d19fe104333e1
AXiON <marcelroodt@gmail.com>
2017-09-24 19:48:14 +0200

    Fixed Crosshair Update
    The crosshair felt super laggy on update. This should now feel much better.
    
    Also removed the input string from the player UI and hid the mana bar. Will refactor the rest when we figure out exactly how it should look.

--- Assets/Scripts/UI/CrosshairPositioner.cs
+++ Assets/Scripts/UI/CrosshairPositioner.cs
@@ -1,40 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CrosshairPositioner : MonoBehaviour {
 
-    public Sprite[] spellCrosshairs;
+	public Sprite[] spellCrosshairs;
 
-    private Camera mainCamera;
-    private const float scale = 0.5f;
-    private const float maxDistance = 30f; //This can change depending on which spell we have
+	private Camera mainCamera;
+	private const float scale = 0.5f;
+	private const float maxDistance = 30f; //This can change depending on which spell we have
 	// Use this for initialization
 	void Start ()
-    {
-        mainCamera = Camera.main;
+	{
+		mainCamera = Camera.main;
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
-        UpdateCrosshair();
+	void LateUpdate ()
+	{
+		UpdateCrosshair();
 	}
 
-    void UpdateCrosshair()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, maxDistance))
-        {
-            transform.position = hit.point + (0.2f * hit.normal);
-            transform.forward = hit.normal;
-            //transform.localScale = new Vector3(scale, scale, scale);
-        }
-        else
-        {
-            transform.position = mainCamera.transform.position + mainCamera.transform.forward * maxDistance;
-            transform.forward = -mainCamera.transform.forward;
-            //transform.localScale = Vector3.zero;
-        }
-    }
+	void UpdateCrosshair()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, maxDistance))
+		{
+			transform.position = hit.point + (0.2f * hit.normal);
+			transform.forward = hit.normal;
+			//transform.localScale = new Vector3(scale, scale, scale);
+		}
+		else
+		{
+			transform.position = mainCamera.transform.position + mainCamera.transform.forward * maxDistance;
+			transform.forward = -mainCamera.transform.forward;
+			//transform.localScale = Vector3.zero;
+		}
+	}
 }
